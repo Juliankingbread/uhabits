f91f3bb9205daab5ec9f1789a1f93f9ad25279cd
Juliankingbread <33343194+Juliankingbread@users.noreply.github.com>
2019-06-22 19:55:51 +0200

    Change some class variables to instance variables

--- app/src/main/java/com/android/datetimepicker/date/DatePickerDialog.java
+++ app/src/main/java/com/android/datetimepicker/date/DatePickerDialog.java
@@ -75,8 +75,8 @@ public class DatePickerDialog extends DialogFragment implements
     private static final int ANIMATION_DURATION = 300;
     private static final int ANIMATION_DELAY = 500;
 
-    private static SimpleDateFormat YEAR_FORMAT = new SimpleDateFormat("yyyy", Locale.getDefault());
-    private static SimpleDateFormat DAY_FORMAT = new SimpleDateFormat("dd", Locale.getDefault());
+    private SimpleDateFormat YEAR_FORMAT = new SimpleDateFormat("yyyy", Locale.getDefault());
+    private SimpleDateFormat DAY_FORMAT = new SimpleDateFormat("dd", Locale.getDefault());
 
     private final Calendar mCalendar = Calendar.getInstance();
     private OnDateSetListener mCallBack;
